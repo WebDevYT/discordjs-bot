1a88e7a0e7f41976062f5d8e5912d6471b1a531e
Web <thedavidts@gmail.com>
2021-03-30 19:32:58 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# discordjs-discord-eggbot
+A "GitHub" Repository for the Discord.js "Discord" bot named "Egg Bot" developed by Web
---
+++ index.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const fs = require('fs');
+
+// Import & require the discord.js module
+const Discord = require('discord.js');
+
+// Create an instance of a Discord client
+// create a new Discord client
+const client = new Discord.Client();
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    // set a new item in the Collection
+    // with the key as the command name and the value as the exported module
+    client.commands.set(command.name, command);
+}
+
+const cooldowns = new Discord.Collection();
+
+// `client.on('...')` events and such below this point
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+    console.log('Ready!');
+    client.user.setActivity('Sub 2 Web on YT');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+    console.log(message.content);
+    // client.on('message', message => {
+    if (!message.content.startsWith(process.env.Prefix) || message.author.bot) return;
+
+    const args = message.content.slice(process.env.Prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+    // the rest of your code
+
+    const command = client.commands.get(commandName)
+        || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+    if (!command) return;
+
+    if (command.args && !args.length) {
+        return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+    }
+
+    if (command.guildOnly && message.channel.type === 'dm') {
+        return message.reply('I can\'t execute that command inside DMs!');
+    }
+
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+        }
+    }
+
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
+});
+
+// Log our bot in using the token from https://discord.com/developers/applications
+// login to Discord with your app's token
+client.login(process.env.Token);
